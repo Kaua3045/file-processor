25f7896c2d183a227891881d5b3266cba45f4e58
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-01 18:36:39 -0300

    feat(file-storage-repository): added the file storage repository interface

---
+++ application/src/main/java/com/kaua/file/processor/application/repository/FileStorageRepository.java
@@ -0,0 +1,12 @@
+package com.kaua.file.processor.application.repository;
+
+import com.kaua.file.processor.domain.importjob.StoredFile;
+
+import java.io.InputStream;
+
+public interface FileStorageRepository {
+
+    StoredFile store(String fileName, InputStream content);
+
+    void delete(String fileRef);
+}
