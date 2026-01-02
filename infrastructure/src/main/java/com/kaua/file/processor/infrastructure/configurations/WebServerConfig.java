aaf9d85807763a05c6faf1a2f32dce13d27b53a8
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-02 15:05:07 -0300

    refactor(change-names): file processor

--- infrastructure/src/main/java/com/kaua/file/processor/infrastructure/configurations/WebServerConfig.java
+++ infrastructure/src/main/java/com/kaua/file/processor/infrastructure/configurations/WebServerConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-@ComponentScan(basePackages = "com.kaua.template")
+@ComponentScan(basePackages = "com.kaua.file.processor")
 public class WebServerConfig {
 }
