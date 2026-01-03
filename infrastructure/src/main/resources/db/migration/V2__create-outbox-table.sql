7e82ca5767b3a656c6e9fd996715d562a3b7c03d
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-03 12:06:54 -0300

    feat(outbox-migrations): added the create outbox table migration

---
+++ infrastructure/src/main/resources/db/migration/V2__create-outbox-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE outbox (
+    id VARCHAR(26) NOT NULL PRIMARY KEY,
+    version BIGINT NOT NULL,
+    aggregate_id VARCHAR(26) NOT NULL,
+    event_type VARCHAR(255) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    payload TEXT NOT NULL,
+    occurred_on TIMESTAMP WITH TIME ZONE NOT NULL
+);
