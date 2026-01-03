c0cbbc41c9a95a0e03c18fcaaa19a3f812d71c02
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-03 19:06:12 -0300

    feat(outbox-repository): added the outbox of status and occuredOn, mark as completed and mark as failed methods

--- infrastructure/src/main/java/com/kaua/file/processor/infrastructure/outbox/OutboxRepository.java
+++ infrastructure/src/main/java/com/kaua/file/processor/infrastructure/outbox/OutboxRepository.java
@@ -1,6 +1,14 @@
 package com.kaua.file.processor.infrastructure.outbox;
 
+import java.util.List;
+
 public interface OutboxRepository {
 
     OutboxEntity save(OutboxEntity entity);
+
+    OutboxEntity markAsCompleted(OutboxEntity entity);
+
+    OutboxEntity markAsFailed(OutboxEntity entity);
+
+    List<OutboxEntity> outboxOfStatusAndOccurredOn(OutboxStatus status);
 }
