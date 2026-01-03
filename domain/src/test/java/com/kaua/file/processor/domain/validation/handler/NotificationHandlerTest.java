3cc3a8166f6e47099007f731b9b6a096f0566e60
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-02 21:28:34 -0300

    test(notification-handler): change template package to correct package

--- domain/src/test/java/com/kaua/file/processor/domain/validation/handler/NotificationHandlerTest.java
+++ domain/src/test/java/com/kaua/file/processor/domain/validation/handler/NotificationHandlerTest.java
@@ -88,7 +88,7 @@ class NotificationHandlerTest extends UnitTest {
 
     @Test
     void givenAValidValidation_whenCallValidate_thenShouldThrowsException() {
-        final var expectedErrorMessage = "Cannot invoke \"com.kaua.template.domain.validation.ValidationHandler$Validation.validate()\" because \"aValidation\" is null";
+        final var expectedErrorMessage = "Cannot invoke \"com.kaua.file.processor.domain.validation.ValidationHandler$Validation.validate()\" because \"aValidation\" is null";
         NotificationHandler handler = NotificationHandler.create();
 
         Assertions.assertDoesNotThrow(() -> handler.validate(null));
