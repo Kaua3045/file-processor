7fdfe4229b168f20141fe7752729d6f92c604255
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-06 09:44:09 -0300

    feat(web-server-config): added the enable scheduling

--- infrastructure/src/main/java/com/kaua/file/processor/infrastructure/configurations/WebServerConfig.java
+++ infrastructure/src/main/java/com/kaua/file/processor/infrastructure/configurations/WebServerConfig.java
@@ -2,8 +2,10 @@ package com.kaua.file.processor.infrastructure.configurations;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration(proxyBeanMethods = false)
 @ComponentScan(basePackages = "com.kaua.file.processor")
+@EnableScheduling
 public class WebServerConfig {
 }
