a2987f96917dcd7392b67a379fec4322c9555a09
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-13 12:14:36 -0300

    feat(get-import-job-progress-usecase): added the get import job progress command, output, usecase abstract class and implementation

---
+++ application/src/main/java/com/kaua/file/processor/application/importjob/progress/GetImportJobProgressUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.file.processor.application.importjob.progress;
+
+import com.kaua.file.processor.application.UseCase;
+
+public abstract class GetImportJobProgressUseCase extends UseCase<GetImportJobProgressCommand, GetImportJobProgressOutput> {
+}
