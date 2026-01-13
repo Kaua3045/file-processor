a2695e328d9666576d058bd2de5129ce7d13cea7
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-13 12:19:41 -0300

    feat(import-job-api): added the get import job status route method

--- infrastructure/src/main/java/com/kaua/file/processor/infrastructure/rest/ImportJobAPI.java
+++ infrastructure/src/main/java/com/kaua/file/processor/infrastructure/rest/ImportJobAPI.java
@@ -1,15 +1,14 @@
 package com.kaua.file.processor.infrastructure.rest;
 
 import com.kaua.file.processor.infrastructure.importjob.res.CreateImportJobResponse;
+import com.kaua.file.processor.infrastructure.importjob.res.GetImportJobProgressResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "Import Job", description = "Import Job API")
@@ -28,4 +27,16 @@ public interface ImportJobAPI {
             @ApiResponse(responseCode = "500", description = "Internal server error")
     })
     ResponseEntity<CreateImportJobResponse> importFile(@RequestPart MultipartFile file);
+
+    @GetMapping(
+            value = "/{importJobId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Get the status of an import job")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ImportJob retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "ImportJob not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    ResponseEntity<GetImportJobProgressResponse> getImportJobStatus(@PathVariable("importJobId") String importJobId);
 }
