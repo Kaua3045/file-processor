023bbf57c32d3bcfee12d1ef68230be2330e5185
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-13 18:43:15 -0300

    feat(event-bus-config): added the kafka event bus bean with configuration condition on property is kafka

--- infrastructure/src/main/java/com/kaua/file/processor/infrastructure/configurations/EventBusConfig.java
+++ infrastructure/src/main/java/com/kaua/file/processor/infrastructure/configurations/EventBusConfig.java
@@ -2,10 +2,13 @@ package com.kaua.file.processor.infrastructure.configurations;
 
 import com.kaua.file.processor.infrastructure.services.eventbus.ApplicationEventBus;
 import com.kaua.file.processor.infrastructure.services.eventbus.EventBus;
+import com.kaua.file.processor.infrastructure.services.eventbus.KafkaEventBus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration(proxyBeanMethods = false)
 public class EventBusConfig {
@@ -14,5 +17,11 @@ public class EventBusConfig {
     @ConditionalOnProperty(name = "application.eventbus", havingValue = "in-memory", matchIfMissing = true)
     public EventBus inMemoryEventBus(final ApplicationContext applicationContext) {
         return new ApplicationEventBus(applicationContext);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "application.eventbus", havingValue = "kafka")
+    public EventBus kafkaEventBus(final KafkaTemplate<String, Object> kafkaTemplate, @Value("${kafka.topics.events}") final String topic) {
+        return new KafkaEventBus(kafkaTemplate, topic);
     }
 }
