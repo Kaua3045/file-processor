777ee0c9c13651c572e9f41173383a5978415e26
Kauã Pereira <zkauacodes@hotmail.com>
2026-01-14 16:11:08 -0300

    test(abstract-repository-test): added the processed event repository

--- infrastructure/src/test/java/com/kaua/file/processor/AbstractRepositoryTest.java
+++ infrastructure/src/test/java/com/kaua/file/processor/AbstractRepositoryTest.java
@@ -4,6 +4,8 @@ import com.kaua.file.processor.application.repository.ImportJobRepository;
 import com.kaua.file.processor.infrastructure.jdbc.JdbcClientAdapter;
 import com.kaua.file.processor.infrastructure.outbox.OutboxJdbcRepository;
 import com.kaua.file.processor.infrastructure.outbox.OutboxRepository;
+import com.kaua.file.processor.infrastructure.processedEvents.ProcessedEventJdbcRepository;
+import com.kaua.file.processor.infrastructure.processedEvents.ProcessedEventRepository;
 import com.kaua.file.processor.infrastructure.repositories.ImportJobJdbcRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -21,6 +23,7 @@ public abstract class AbstractRepositoryTest {
 
     private static final String IMPORTS_TABLE = "import_jobs";
     private static final String OUTBOX_TABLE = "outbox";
+    private static final String PROCESSED_EVENTS_TABLE = "processed_events";
 
     @Autowired
     private JdbcClient jdbcClient;
@@ -30,11 +33,13 @@ public abstract class AbstractRepositoryTest {
 
     private ImportJobRepository importJobJdbcRepository;
     private OutboxRepository outboxRepository;
+    private ProcessedEventRepository processedEventRepository;
 
     @BeforeEach
     void setUp() {
         this.outboxRepository = new OutboxJdbcRepository(new JdbcClientAdapter(jdbcClient, operations));
         this.importJobJdbcRepository = new ImportJobJdbcRepository(new JdbcClientAdapter(jdbcClient, operations), outboxRepository);
+        this.processedEventRepository = new ProcessedEventJdbcRepository(new JdbcClientAdapter(jdbcClient, operations));
     }
 
     protected int countImportJobs() {
@@ -45,11 +50,19 @@ public abstract class AbstractRepositoryTest {
         return JdbcTestUtils.countRowsInTable(jdbcClient, OUTBOX_TABLE);
     }
 
+    protected int countProcessedEvents() {
+        return JdbcTestUtils.countRowsInTable(jdbcClient, PROCESSED_EVENTS_TABLE);
+    }
+
     public ImportJobRepository importJobRepository() {
         return importJobJdbcRepository;
     }
 
     public OutboxRepository outboxRepository() {
         return outboxRepository;
+    }
+
+    public ProcessedEventRepository processedEventRepository() {
+        return processedEventRepository;
     }
 }
